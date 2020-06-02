31954410d7fc0785d19992d7b221cc7b583c7720
gabrielCodeSolutions <gabrielunitone@gmail.com>
2020-06-02 20:51:13 -0300

    Adicionado métodos post e get para teste

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,48 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', (request,response)=>{
-  console.log('Listagem de usuários')
+app.use(express.json())
+// Rota é o endereço completo da nossa requisição;
 
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel',
-    'Gabriel'
-  ])
+// Recurso: Qual entidade estamos acessando do sistema.
+
+//GET Buscar uma ou mais informações do back-end
+//POST Criar uma informação no back-end
+//PUT: Atualizar uma informação existentee
+
+// Request Param: Parâmetro que vem na própria rota que identificam um return
+// Query Param: Parâmetros que vem na rota geralmente opcionais para filtros, paginação e etc...
+// Request Body: São parâmetros para criação e atualização de informações;
+const users = [
+  'Diego',
+  'Cleiton',
+  'Robson',
+  'Daniel',
+  'Gabriel'
+]
+app.get('/users', (request,response)=>{
+  const search = String(request.query.search)
+
+  const filteredUsers = search ? users.filter(user=> user.includes(search)) : users
+
+  return response.json(filteredUsers)
+})
+app.get('/users/:id',(request,response)=>{
+  const id = Number(request.params.id)
+
+  const user = users[id]
+
+  return response.json(user)
+})
+app.post('/users', (request,response)=>{
+  const data = request.body
+
+  const user ={
+    name:data.name,
+    email:data.email
+  }
+
+  return response.json(user)
 })
 
 app.listen(3333)

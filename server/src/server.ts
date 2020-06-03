f9f2fa4e45720183274cb1b7882869e70edaeaed
gabrielCodeSolutions <gabrielunitone@gmail.com>
2020-06-02 21:47:26 -0300

    Adicionado gitignore e as primeiras tabelas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,11 @@
 import express from 'express'
-
+import routes from './routes'
 const app = express()
 
 app.use(express.json())
-// Rota é o endereço completo da nossa requisição;
 
-// Recurso: Qual entidade estamos acessando do sistema.
+app.use(routes)
 
-//GET Buscar uma ou mais informações do back-end
-//POST Criar uma informação no back-end
-//PUT: Atualizar uma informação existentee
-
-// Request Param: Parâmetro que vem na própria rota que identificam um return
-// Query Param: Parâmetros que vem na rota geralmente opcionais para filtros, paginação e etc...
-// Request Body: São parâmetros para criação e atualização de informações;
 const users = [
   'Diego',
   'Cleiton',
@@ -21,29 +13,9 @@ const users = [
   'Daniel',
   'Gabriel'
 ]
-app.get('/users', (request,response)=>{
-  const search = String(request.query.search)
+app.get('/', (request,response)=>{
 
-  const filteredUsers = search ? users.filter(user=> user.includes(search)) : users
-
-  return response.json(filteredUsers)
-})
-app.get('/users/:id',(request,response)=>{
-  const id = Number(request.params.id)
-
-  const user = users[id]
-
-  return response.json(user)
-})
-app.post('/users', (request,response)=>{
-  const data = request.body
-
-  const user ={
-    name:data.name,
-    email:data.email
-  }
-
-  return response.json(user)
+  return response.json({message:'Hello World'})
 })
 
 app.listen(3333)
